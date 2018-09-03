e75aec71f9d99f3fc3d95e9a3e8c87f74d7d6f37
tupers <glamorousdd@sina.com>
2018-09-03 09:24:13 +0000

    add scenario tool class

---
+++ include/scenarioTool.h
@@ -0,0 +1,17 @@
+#ifndef SCENARIOTOOL_H
+#define SCENARIOTOOL_H
+
+class ScenarioTool
+{
+private:
+	//scenario
+	//action
+	//keywords
+	//environment
+
+public:
+	ScenarioTool();
+	virtual ~ScenarioTool();
+};
+
+#endif /* SCENARIOTOOL_H */
